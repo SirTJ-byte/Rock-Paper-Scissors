84d42d5a69c3b23313a1bd3ffe3b700f7222d1c3
SirTJ-byte <217216463+SirTJ-byte@users.noreply.github.com>
2025-07-17 05:54:58 +0200

    reworked my code separetly on another clone and put it here

--- script.js
+++ script.js
@@ -1,64 +1,76 @@
-function playGame() {
-	let array = ["rock", "paper", "scissors"];
+const inputField = document.getElementById("fieldInput");
+const play = document.getElementById("play");
+const resultsOfGame = document.getElementById("EachGameResult");
+const finalResult = document.getElementById("finalResult");
+const reset = document.getElementsByClassName("restart");
+const selection = document.getElementById("invalidSelection");
 
-	let playerScore = 0;
-	let computerScore = 0;
+const player = document.querySelector(".playerScore span");
+const computer = document.querySelector(".computerScore span");
 
-	function computersChoice() {
-		const computerRandomChoice = Math.floor(Math.random() * array.length);
-		const computerChooses = array[computerRandomChoice];
-		return computerChooses;
-	}
+let array = ["rock", "paper", "scissors"];
 
-	for (let getRound = 1; getRound <= 5; getRound++) {
-		let playerChoice;
-		let isInvalidInput = false;
-		while (!isInvalidInput) {
-			playerChoice = prompt("Choose between Rock, Paper, and Scissors")
-				.toLowerCase()
-				.trim();
-			//check the vadility
-			if (array.includes(playerChoice)) {
-				isInvalidInput = true;
-			} else {
-				console.log("invalid choice!!! Please Choose Rock, Paper and Scissors");
-			}
-		}
-		const computerChoose = computersChoice();
+let playerScore = 0;
+let computerScore = 0;
 
-		console.log(`Player Chooses : ${playerChoice}`);
-		console.log(`Computer Chooses: ${computerChoose}`);
-
-		if (playerChoice === computerChoose) {
-			console.log("its a tie");
-		} else if (
-			(playerChoice === array[1] && computerChoose === array[0]) ||
-			(playerChoice === array[2] && computerChoose === array[1]) ||
-			(playerChoice === array[0] && computerChoose === array[2])
-		) {
-			playerScore++;
-			console.log("Player Wins!!!");
-		} else {
-			computerScore++;
-			console.log("Computer Wins!!!");
-		}
-
-		console.log(`round ${getRound} complete`);
-		console.log(
-			`current Score : Player ${playerScore} vs Computer${computerScore}`
-		);
-		console.log("-------------------------");
-	}
-	console.log("\n --- Final results ---");
-	console.log(
-		`Final Score: Player ${playerScore} vs Computer ${computerScore}`
-	);
-	if (playerScore > computerScore) {
-		console.log("Congrats, You Win");
-	} else if (playerScore < computerScore) {
-		console.log("Sorry, Computer Won");
-	} else {
-		console.log("The game is a Grand tie!");
-	}
+function computersChoice() {
+	const computerRandomChoice = Math.floor(Math.random() * array.length);
+	const computerChooses = array[computerRandomChoice];
+	return computerChooses;
 }
-playGame();
+
+function playGame() {
+	if (playerScore >= 3 || computerScore >= 3) {
+		if (playerScore > computerScore) {
+			finalResult.textContent = `current Score : Player ${playerScore} vs Computer${computerScore} Congrats, You Win`;
+		} else if (playerScore < computerScore) {
+			finalResult.textContent = `current Score : Player ${playerScore} vs Computer${computerScore} Sorry, Computer Won`;
+		}
+
+		console.log("\n --- Final results ---");
+		console.log(
+			`Final Score: Player ${playerScore} vs Computer ${computerScore}`
+		);
+		return;
+	}
+
+	let playerChoice = inputField.value.toLowerCase().trim();
+	if (array.includes(playerChoice)) {
+		selection.textContent = "";
+	} else {
+		selection.textContent =
+			"invalid choice!!! Please Choose Rock, Paper and Scissors";
+		return;
+	}
+
+	const computerChoose = computersChoice();
+
+	console.log(`Player Chooses : ${playerChoice}`);
+	console.log(`Computer Chooses: ${computerChoose}`);
+
+	if (playerChoice === computerChoose) {
+		resultsOfGame.textContent = "its a tie";
+	} else if (
+		(playerChoice === array[1] && computerChoose === array[0]) ||
+		(playerChoice === array[2] && computerChoose === array[1]) ||
+		(playerChoice === array[0] && computerChoose === array[2])
+	) {
+		playerScore++;
+
+		resultsOfGame.textContent = "Player Wins!!!";
+	} else {
+		computerScore++;
+		resultsOfGame.textContent = "Computer Wins!!!";
+	}
+	player.textContent = playerScore;
+	computer.textContent = computerScore;
+}
+function restart() {
+	playerScore = 0;
+	computerScore = 0;
+	resultsOfGame.textContent = "";
+	finalResult.textContent = "";
+}
+
+play.addEventListener("click", playGame);
+reset[0].addEventListener("click", restart);
